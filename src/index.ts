63fd5089f83abc71986f1eae532aa74664355b4e
Aumer <aumer@aumer.dev>
2022-11-21 18:46:02 +0100

    feat(bot): Added default presence

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './bootstrap';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { ActivityType, Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
@@ -7,6 +7,13 @@ const client = new Client({
 
 client.once('ready', () => {
     console.log('Ready!');
+    client.user?.setPresence({
+        activities: [{
+            name: 'EVE Online',
+            type: ActivityType.Playing
+        }],
+        status: 'dnd'
+    });
 });
 
 client.login(process.env.DISCORD_TOKEN);
